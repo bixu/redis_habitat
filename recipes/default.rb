56f8464b13500eaf44103151f80fad9bc35bf89d
Blake Irvin <blake.irvin@gmail.com>
2016-11-10 22:11:14 +0100

    initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,30 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+hab_install 'install habitat'
+
+user 'hab' do
+  action :create
+end
+
+hab_package "core/redis" do
+  action :install
+end
+
+hab_service 'core/redis' do
+  action [:enable, :start]
+  unit_content <<-EOF
+[Unit]
+Description=redis
+
+[Service]
+Environment = "HAB_REDIS=tcp-backlog=128"
+ExecStart = /bin/hab start core/redis
+Restart=on-failure
+
+[Install]
+WantedBy = default.target
+
+EOF
+end
--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -8,11 +8,18 @@
 unless os.windows?
   describe user('root') do
     it { should exist }
-    skip 'This is an example test, replace with your own test.'
   end
 end
 
-describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+describe port(6379) do
+  it { should be_listening }
+end
+
+describe command('hab pkg exec core/redis redis-cli ping') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should match('PONG') }
+end
+
+describe command('hab --version') do
+  its('exit_status') { should eq 0 }
 end
